c2b2d0218a1a085a3b56e51aca1fc6c6f464c4e2
JagdishKumar2023 <jagdishkumarsingh2023@gmail.com>
2024-01-04 23:21:21 +0530

    New Increment Decerment Project for reducx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./index.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className="container">
+      <h1>Increment/Decrement counter</h1>
+      <h4>using React and Redux</h4>
+
+      <div class="quantity">
+        <a class="quantity_plus" title="Increment">
+          <button>+</button>
         </a>
-      </header>
+        <a class="quantity_minus" title="Decrement">
+          <button>-</button>
+        </a>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/Reducer/Store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux";
+
+import rootReducer from "./Reducerupdown";
+
+const store = createStore(rootReducer);
